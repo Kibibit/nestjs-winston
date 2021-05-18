a85a053e8ea949db5f5b6a8c4dee12c36a67913f
Neil Kalman <Neilkalman@gmail.com>
2021-05-18 17:39:28 +0300

    feat(logger): optional default meta
    instead of just initializing the context on initialization, allow passing an object as default metadata

--- src/winston.logger.ts
+++ src/winston.logger.ts
@@ -1,74 +1,78 @@
 import { Logger } from '@nestjs/common';
 
 export class WinstonLogger {
-    private readonly logger: Logger;
+  private readonly logger: Logger;
+  private defaultMeta: Record<string, unknown> = {};
 
-    /**
-     * Creates an instance of WinstonLogger.
-     * @param {string} context
-     * @memberof WinstonLogger
-     */
-    constructor(context: string) {
-        this.logger = new Logger(context, true);
-    }
-    /**
-     * Log an info message
-     *
-     * @param {string} message
-     * @param {object} meta
-     * @memberof WinstonLogger
-     */
-    public log(message: string, meta: object = {}) {
-        this.logger.log({ message, ...meta});
-    }
+  /**
+   * Creates an instance of WinstonLogger.
+   */
+  constructor(contextOrMeta: string, defaultMeta: Record<string, unknown>)
+  constructor(contextOrMeta: Record<string, unknown>)
+  constructor(contextOrMeta: string | Record<string, unknown>, defaultMeta?: Record<string, unknown>) {
+    const context = typeof contextOrMeta === 'string' ? contextOrMeta : contextOrMeta.context as string | undefined;
+    this.defaultMeta = typeof contextOrMeta === 'object' ? contextOrMeta : (defaultMeta || {});
 
-    /**
-     * Log an info message
-     * @param {string} message
-     * @param {object} meta
-     * @memberof WinstonLogger
-     */
-    public info(message: string, meta: object = {}) {
-        this.logger.log({ message, ...meta});
-    }
+    this.logger = new Logger(context, true);
+  }
+  /**
+   * Log an info message
+   *
+   * @param {string} message
+   * @param {object} meta
+   * @memberof WinstonLogger
+   */
+  public log(message: string, meta: object = {}) {
+    this.logger.log({ message, ...meta, ...this.defaultMeta });
+  }
 
-    /**
-     * Log a debug message
-     * @param {string} message
-     * @param {object} meta
-     * @memberof WinstonLogger
-     */
-    public debug(message: string, meta: object = {}) {
-        this.logger.debug({ message, ...meta});
-    }
+  /**
+   * Log an info message
+   * @param {string} message
+   * @param {object} meta
+   * @memberof WinstonLogger
+   */
+  public info(message: string, meta: object = {}) {
+    this.logger.log({ message, ...meta, ...this.defaultMeta });
+  }
 
-    /**
-     * Log a verbose message
-     * @param {string} message
-     * @param {object} meta
-     * @memberof WinstonLogger
-     */
-    public verbose(message: string, meta: object = {}) {
-        this.logger.verbose({ message, ...meta});
-    }
+  /**
+   * Log a debug message
+   * @param {string} message
+   * @param {object} meta
+   * @memberof WinstonLogger
+   */
+  public debug(message: string, meta: object = {}) {
+    this.logger.debug({ message, ...meta });
+  }
 
-    /**
-     * Log an error message
-     * @param {string} message
-     * @param {object} meta
-     * @memberof WinstonLogger
-     */
-    public error(message: string, meta: object = {}) {
-        this.logger.error({ message, ...meta});
-    }
+  /**
+   * Log a verbose message
+   * @param {string} message
+   * @param {object} meta
+   * @memberof WinstonLogger
+   */
+  public verbose(message: string, meta: object = {}) {
+    this.logger.verbose({ message, ...meta, ...this.defaultMeta });
+  }
 
-    /**
-     * Log a warn message
-     * @param {string} message
-     * @param {object} meta
-     * @memberof WinstonLogger
-     */
-    public warn(message: string, meta: object = {}) {
-        this.logger.warn({ message, ...meta});
-    }
+  /**
+   * Log an error message
+   * @param {string} message
+   * @param {object} meta
+   * @memberof WinstonLogger
+   */
+  public error(message: string, meta: object = {}) {
+    this.logger.error({ message, ...meta, ...this.defaultMeta });
+  }
+
+  /**
+   * Log a warn message
+   * @param {string} message
+   * @param {object} meta
+   * @memberof WinstonLogger
+   */
+  public warn(message: string, meta: object = {}) {
+    this.logger.warn({ message, ...meta, ...this.defaultMeta });
+  }
 }
